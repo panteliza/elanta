8fd02c95ef5332affd5257d330148f685f3b42ac
Eliza Pant <elizapant.github@gmail.com>
2023-08-11 18:38:48 +0545

    update navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,10 +4,17 @@ import abc from '../assets/butterfly.jpg';
 import { BsFacebook, BsInstagram } from 'react-icons/bs';
 import { AiOutlineDown, AiOutlineUp } from 'react-icons/ai';
 import { HiOutlineMenuAlt2 } from 'react-icons/hi';
+import { MdOutlineClose } from 'react-icons/md';
 
 const Navbar = () => {
   const [showDropdown, setShowDropdown] = useState(false);
+  const[show,setshow]=useState(false)
   const navigate = useNavigate(); // Get the navigate function from useNavigate
+
+
+  function showIt(){
+    setshow(!show)
+  }
 
   const toggleDropdown = () => {
     setShowDropdown(!showDropdown);
@@ -76,12 +83,94 @@ const Navbar = () => {
           <div className="hover:text-blue-500 hover:bg-pink-200 px-2 py-2 rounded-md">ABOUT</div>
           <div className="hover:text-blue-500 hover:bg-pink-200 px-2 py-2 rounded-md">CONTACT</div>
         </div>
-        <div className='lg:hidden text-gray-500    '>
-          <HiOutlineMenuAlt2 className=' text-[40px] '/>
-        </div>
+{/* sidebar */}
+{show && (
+  <div className="lg:hidden flex flex-col w-[300px] top-0 fixed h-full left-0 gap-2 bg-[#F4F4F4]">
+    {/* ...previous sidebar content... */}
+    <div className="flex justify-end pr-4 py-3 ">
+      <MdOutlineClose className="text-[30px]" onClick={showIt} />
+    </div>
+    <div className="flex gap-2  ">
+      <div className="flex justify-center items-center w-[80px] pl-5px  ">
+        <img src={abc} alt="logo" className="h-[50px] w-[50px] rounded-full" />
+      </div>
+      <div className="font-semibold text-lg">Elanta Health Care & Hygiene Prducts Pvt.Ltd.</div>
+    </div>
 
-        <div className="transition-transform duration-200 transform hover:scale-125 cursor-pointer  hidden md:block lg:hidden">
-  <img src={abc} alt="logo" className="h-[60px] w-[60px] rounded-full" />
+    <div
+      className={`border border-gray-400 relative   flex gap-2 text-gray-600 items-center w-[250px] justify-center  py-2 ${showDropdown ? 'mb-[200px]' : 'mb-[0px]'}`}
+      onMouseEnter={toggleDropdown}
+      onMouseLeave={toggleDropdown}
+    >
+      <div className="">PRODUCTS</div>
+      <div className="text-[15px] font-bold 2xl:text-[30px]">
+        {showDropdown ? <AiOutlineUp /> : <AiOutlineDown />}
+      </div>
+      {showDropdown && (
+        <div className="absolute top-12  ">
+          <div className="flex flex-col gap-3 pt-2 pb-4">
+            {/* Use the handleAllProductsClick function when clicking on the link */}
+            <div className="border border-gray-400  flex gap-2 text-gray-600 items-center w-[250px] justify-center  py-2 " onClick={handleAllProductsClick}>
+              All Products
+            </div>
+            <div className="border border-gray-400  flex gap-2 text-gray-600 items-center w-[250px] justify-center  py-2" onClick={handleSanitaryPadsClick}>
+              Sanitary Pads
+            </div>
+            <div className="border border-gray-400  flex gap-2 text-gray-600 items-center w-[250px] justify-center  py-2" onClick={handleAdultDiapersClick}>
+              Adult Diapers
+            </div>
+            <div className="border border-gray-400  flex gap-2 text-gray-600 items-center w-[250px] justify-center  py-2" onClick={handleBabyDiapersClick}>
+              Baby Diapers
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+
+    <div
+      className={`border border-gray-400  flex gap-2 text-gray-600 items-center w-[250px] justify-center  py-2 ${
+        showDropdown ? 'mt-[25px]' : 'mt-[0px]'
+      }`}
+    >
+      <div>About</div>
+    </div>
+
+
+<div className='border border-gray-400  flex gap-2 text-gray-600 
+items-center w-[250px] justify-center  py-2'>
+  <div >Media</div>
+  </div>
+
+  <div className='border border-gray-400  flex gap-2 text-gray-600 
+items-center w-[250px] justify-center py-2'>
+  <div >Conatct</div>
+  </div>
+
+<div className='flex ml-[100px]  gap-2 text-pink-400 cursor-pointer text-[25px]  '>
+    <BsFacebook className="hover:text-pink-600" />
+    <BsInstagram className="hover:text-pink-600" />
+</div>
+
+<div className='bg-pink-400 rounded-3xl mt-[80px] px-12 py-2 text-white
+ text-[18px] w-[260px]'>
+  <div>Dealership Enquiry</div>
+  
+</div>
+
+  </div>
+        
+        )}
+
+
+
+<div className='lg:hidden text-gray-500    '>
+  <HiOutlineMenuAlt2 className=' text-[40px] ' onClick={showIt}/>
+</div>
+
+<div className="transition-transform duration-200 transform hover:scale-125 cursor-pointer  hidden md:block lg:hidden">
+  
+<img src={abc} alt="logo" className="h-[60px] w-[60px] rounded-full" />
+
 </div>
 
 
